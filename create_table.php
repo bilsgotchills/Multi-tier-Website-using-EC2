d6b8ef37b674d7d4fee7073a5f85c23a0eac3a9e
bilsgotchills <bilsnr0@gmail.com>
2025-10-05 18:57:33 +0530

    Create create_table.php

---
+++ create_table.php
@@ -0,0 +1,27 @@
+
+<?php
+$servername = "<your_rds_endpoint>";
+$username = "admin";
+$password = "<yourdbpassword>";
+$dbname = "mydbname>";
+
+$tableName = $_GET['name'];
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $sql = "CREATE TABLE IF NOT EXISTS $tableName (
+        id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(30) NOT NULL,
+        email VARCHAR(50) NOT NULL
+    )";
+
+    $conn->exec($sql);
+    echo "Table $tableName created successfully";
+} catch (PDOException $e) {
+    echo "Error creating table: " . $e->getMessage();
+}
+
+$conn = null;
+?>
